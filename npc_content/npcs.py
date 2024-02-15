844794d1f41ca122de3133bcde696ab7cfb298d5
Carl Corneil <aecorn@gmail.com>
2024-02-15 23:49:51 +0100

    more npcs, prompt changes

--- npc_content/npcs.py
+++ npc_content/npcs.py
@@ -40,22 +40,25 @@ def get_world_data():
 def setup_npcs(location: str = "Prinberg"):
 
     content = get_world_data()
-
+    descriptions = {}
+    descriptions["world"] = content["world_info_prompt"]
     combined = {}
     for location, factions in content.items():
+        descriptions["location"] = content[location].get("description")
         if isinstance(factions, dict):
             for faction, characters in factions.items():
+                descriptions["faction"] = factions[faction].get("description")
                 #print(faction)
                 #print(characters)
                 if isinstance(characters, dict):
                     for character, attributes in characters.items():       
-                        attributes["prompt"] = make_prompt(attributes)
+                        attributes["prompt"] = make_prompt(attributes, descriptions)
                         combined[f"{character.lower()}-{faction.lower()}-{location.lower()}"] = attributes
                         #print(attributes["prompt"])
     return combined
 
-def make_prompt(attributes) -> str:
-    prompt = f"""You are a character in the fantasy setting Zarthuga. Where souls do not move on after death, and gods dying has been disasterous for the safety of this worlds peoples.
+def make_prompt(attributes, descriptions) -> str:
+    prompt = f"""{descriptions["world"]} You are currently located in {descriptions["location"]}. You are a member of the faction {descriptions["faction"]}
 You are a {attributes["race"]} named {attributes["full_name"]}, you would be described by the people close to you as: {attributes["description"]}
 When you speak you {attributes["figure_speech"]}"""
     if attributes["relations"]:
@@ -63,7 +66,7 @@ When you speak you {attributes["figure_speech"]}"""
         prompt += f"The people close to you are: {close_relations}"
     if attributes["secrets"]:
         prompt += "They know some of your secrets. You will be very hesitant to reveal your secrets to the person you are talking to. But if you feel confident in the person you are talking to, and they manage to convince you, you may tell them one of your secrets: """ + ". Next secret: ".join(attributes["secrets"]) + ""
-    prompt += "Answer the following conversation as this character."
+    prompt += "Answer the following conversation as the character described earlier, and only as this character. Feel free to describe actions the character does."
     return prompt
 
 PERSONAS = setup_npcs()
